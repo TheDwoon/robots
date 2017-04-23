2ac27488766d3f93424f1eb30c2be8a0c04eb686
TheDwoon <d.wieland@arcor.de>
2017-04-23 14:52:23 +0200

    initial commit

---
+++ src/main/java/schnupperstudium/robots/server/App.java
@@ -0,0 +1,13 @@
+package schnupperstudium.robots.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
