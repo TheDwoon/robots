808b82891bf3308f1256db68830141a1a449c0e2
TheDwoon <d.wieland@arcor.de>
2017-04-23 14:57:56 +0200

    bare structure

---
+++ src/main/java/schnupperstudium/robots/client/RobotsClient.java
@@ -0,0 +1,7 @@
+package schnupperstudium.robots.client;
+
+public final class RobotsClient {
+	public static void main(String[] args) {
+		System.out.println("Client!");
+	}
+}
---
+++ src/main/java/schnupperstudium/robots/server/RobotsServer.java
@@ -0,0 +1,7 @@
+package schnupperstudium.robots.server;
+
+public final class RobotsServer {
+	public static void main(String[] args) {
+		System.out.println("Server!");
+	}
+}
