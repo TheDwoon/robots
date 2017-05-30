fb48e4f54816071a2e5afe50b01b5c940f913484
Sigmar Walter <sigmar-walter@web.de>
2017-05-30 20:12:05 +0200

    changed name of client.student.KI to client.student.AI

---
+++ src/main/java/com/github/TheDwoon/robots/client/student/AI.java
@@ -0,0 +1,16 @@
+package com.github.TheDwoon.robots.client.student;
+
+import com.github.TheDwoon.robots.game.entity.Robot;
+
+public class AI {
+	Robot robot;
+
+	public AI(final Robot robot) {
+		this.robot = robot;
+	}
+
+	// public Action doTurn() {
+	// robot.turnLeft();
+	// robot.getWeaponModule().shoot();
+	// }
+}
