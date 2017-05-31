d1d38f29ae2be3b12def39ac69b2849fa83e20d0
TheDwoon <d.wieland@arcor.de>
2017-05-31 17:30:38 +0200

    quoted non compiling code

--- src/main/java/com/github/TheDwoon/robots/server/BoardInformation.java
+++ src/main/java/com/github/TheDwoon/robots/server/BoardInformation.java
@@ -20,39 +20,39 @@ public class BoardInformation {
 		entities = new ArrayList<>(8);
 	}
 	
-	public static BoardInformation obatinBoardInfoForEntity(Entity entity) {
-		if (entity.getBoard() == null)
-			return null;
-
-		BoardInformation info = new BoardInformation();
-		info.positionX = entity.getX();
-		info.positionY = entity.getY();
-		
-		Board board = entity.getBoard();
-		int x = entity.getX();
-		int y = entity.getY();
-		
-		for (int i = x - 2; i <= x + 2; i++) {
-			for (int j = y - 2; j <= y + 2; j++) {
-				if (i < 0 || j < 0 || i >= board.getWidth() || j >= board.getHeight())
-					continue;
-				
-				int dX = Math.abs(i - entity.getX());
-				int dY = Math.abs(j - entity.getY());
-				if (dX + dY <= 2)
-					info.fields.add(board.getField(i, j));
-			}
-		}
-		
-		for (Entity e : board.getEntities()) {
-			int dX = Math.abs(e.getX() - entity.getX());
-			int dY = Math.abs(e.getY() - entity.getY());
-			if (dX + dY <= 2) 
-				info.entities.add(e);
-		}
-		
-		return info;
-	}
+//	public static BoardInformation obatinBoardInfoForEntity(Entity entity) {
+//		if (entity.getBoard() == null)
+//			return null;
+//
+//		BoardInformation info = new BoardInformation();
+//		info.positionX = entity.getX();
+//		info.positionY = entity.getY();
+//		
+//		Board board = entity.getBoard();
+//		int x = entity.getX();
+//		int y = entity.getY();
+//		
+//		for (int i = x - 2; i <= x + 2; i++) {
+//			for (int j = y - 2; j <= y + 2; j++) {
+//				if (i < 0 || j < 0 || i >= board.getWidth() || j >= board.getHeight())
+//					continue;
+//				
+//				int dX = Math.abs(i - entity.getX());
+//				int dY = Math.abs(j - entity.getY());
+//				if (dX + dY <= 2)
+//					info.fields.add(board.getField(i, j));
+//			}
+//		}
+//		
+//		for (Entity e : board.getEntities()) {
+//			int dX = Math.abs(e.getX() - entity.getX());
+//			int dY = Math.abs(e.getY() - entity.getY());
+//			if (dX + dY <= 2) 
+//				info.entities.add(e);
+//		}
+//		
+//		return info;
+//	}
 	
 	public int getPositionX() {
 		return positionX;
